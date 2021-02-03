81ff7c19e7af75c8955672ca6458cc65d0911a28
Alxdayv <43938766+Alxdayv@users.noreply.github.com>
2021-02-03 20:48:46 +0300

    terminal commit

--- testscript.js
+++ testscript.js
@@ -1,3 +1,23 @@
 'use strict';
 let myHeading = document.querySelector('h1');
-// myHeading.textContent = 'Путин Хуйло!';
+let podmena = prompt('введите подмену');
+myHeading.textContent = podmena;
+console.log(myHeading);
+
+let peremennaya = 10;
+let peremennaya1 = 10;
+console.log(++peremennaya);
+console.log(peremennaya1++);
+/*----------------------
+
++ конкатенация - склеивание объектов
+инкремент ++ увеличивает на 1 если стоит префиксом, то результат сразу, если постфиксом, то результат не сразу 
+декремент -- уменьшает на 1
+оператор И - &&
+ИЛИ - ||
+= присваевает
+== не строгое сравнивание
+=== сравнивание по типу данных
+% - остаток от деления (5%2=1) 
+
+---------------------- */
